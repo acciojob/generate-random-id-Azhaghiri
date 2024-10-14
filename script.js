ec4f5b6a3e6876f6c32d66cf146a42f7b42f0149
Azhaghiri <ganadhan6528@gmail.com>
2024-10-14 19:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var char_list = ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789;
+
+	for (let i=0;i<l;i++) {
+		res += char_list.charAt(
+			math.floor(
+				math.random()*char_list.length
+			)
+		)
+	}
+	return yes
 }
 
 // Do not change the code below.
