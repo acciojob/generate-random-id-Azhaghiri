0ea8c626c12af5d949f48dcbfae75a7e1488132c
Azhaghiri <ganadhan6528@gmail.com>
2025-08-28 07:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let res = '' 
+	let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	for(let i=0;i<l;i++){
+		res += char.charAt(Math.floor(Math.random()*char.length))
+	}
+	return res
 }
 
 // Do not change the code below.
